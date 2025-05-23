39e287343825958d59ec74f223a3d9f6d69f7358
Amol <User@DESKTOP-92CQSJK>
2025-05-23 12:18:55 +0530

    .net Crud Operations Project Added

---
+++ CrudOperations/CrudOperations/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CrudOperations.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudOperations.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> users {  get; set; }
+    }
+}
